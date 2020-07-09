e20874f790a4b1729880829e72f48efe6b2335f5
Caio <caiofcastro@usp.br>
2020-07-09 15:39:46 -0300

    v1.0 (it compiles!)

--- EP2/src/mandelbrot_ompi_cuda.cu
+++ EP2/src/mandelbrot_ompi_cuda.cu
@@ -1,13 +1,16 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 
 #include "mpi.h"
 
 #define min(x, y) (x < y ? x : y);
 #define MASTER 0
 
-#define DEBUG 1
+// #define DEBUG 1
+#define DEBUG 0
 
 struct process_args
 {
@@ -16,6 +19,18 @@ struct process_args
     int start_x;
     int end_x;
 };
+
+struct timer_info
+{
+    clock_t c_start;
+    clock_t c_end;
+    struct timespec t_start;
+    struct timespec t_end;
+    struct timeval v_start;
+    struct timeval v_end;
+};
+
+struct timer_info timer;
 
 double c_x_min;
 double c_x_max;
@@ -33,6 +48,9 @@ unsigned char **image_buffer;
 int i_x_max;
 int i_y_max;
 int image_buffer_size;
+
+int block_dim_x;
+int block_dim_y;
 
 int gradient_size = 16;
 int colors[17][3] = {
@@ -69,21 +87,22 @@ void allocate_image_buffer()
 
 void init(int argc, char *argv[], int rank)
 {
-    if (argc < 6)
+    if (argc < 8)
     {
         if (rank == MASTER)
         {
             printf("usage:    mpirun -np [num_processes] --host localhost:[num_processes]"
-                   " mandelbrot_ompi c_x_min c_x_max c_y_min c_y_max image_size\n");
+                   " mandelbrot_ompi_cuda c_x_min c_x_max c_y_min c_y_max image_size"
+                   " grid_x grid_y\n");
             printf("examples with image_size = 11500:\n");
             printf("    Full Picture:         mpirun -np 9 --host localhost:9 "
-                   "mandelbrot_ompi -2.5 1.5 -2.0 2.0 11500\n");
+                   "mandelbrot_ompi_cuda -2.5 1.5 -2.0 2.0 11500 4 4\n");
             printf("    Seahorse Valley:      mpirun -np 9 --host localhost:9 "
-                   "mandelbrot_ompi  -0.8 -0.7 0.05 0.15 11500 4\n");
+                   "mandelbrot_ompi_cuda  -0.8 -0.7 0.05 0.15 11500 4 4\n");
             printf("    Elephant Valley:      mpirun -np 9 --host localhost:9 "
-                   "mandelbrot_ompi  0.175 0.375 -0.1 0.1 11500 4\n");
+                   "mandelbrot_ompi_cuda  0.175 0.375 -0.1 0.1 11500 4 4\n");
             printf("    Triple Spiral Valley: mpirun -np 9 --host localhost:9 "
-                   "mandelbrot_ompi  -0.188 -0.012 0.554 0.754 11500 4\n");
+                   "mandelbrot_ompi_cuda  -0.188 -0.012 0.554 0.754 11500 4 4\n");
         }
         exit(0);
     }
@@ -94,6 +113,8 @@ void init(int argc, char *argv[], int rank)
         sscanf(argv[3], "%lf", &c_y_min);
         sscanf(argv[4], "%lf", &c_y_max);
         sscanf(argv[5], "%d", &image_size);
+        sscanf(argv[6], "%d", &block_dim_x);
+        sscanf(argv[7], "%d", &block_dim_y);
 
         i_x_max = image_size;
         i_y_max = image_size;
@@ -145,68 +166,6 @@ void write_to_file()
     fclose(file);
 };
 
-int *compute_mandelbrot(struct process_args *process_data, int rank)
-{
-    double z_x;
-    double z_y;
-    double z_x_squared;
-    double z_y_squared;
-    double escape_radius_squared = 4;
-
-    int iteration;
-    int i_x;
-    int i_y;
-    double c_x;
-    double c_y;
-
-    int start_y = process_data->start_y;
-    int end_y = process_data->end_y;
-    int start_x = process_data->start_x;
-    int end_x = process_data->end_x;
-    int *result =
-        (int *)malloc(3 * (end_x - start_x) * (end_y - start_y) * sizeof(int));
-    int counter = 0;
-
-    if (DEBUG)
-        printf("[%d]: %d %d %d %d\n", rank, start_x, end_x, start_y, end_y);
-
-    for (i_y = start_y; i_y < end_y; i_y++)
-    {
-        c_y = c_y_min + i_y * pixel_height;
-
-        if (fabs(c_y) < pixel_height / 2)
-        {
-            c_y = 0.0;
-        };
-
-        for (i_x = start_x; i_x < end_x; i_x++)
-        {
-            c_x = c_x_min + i_x * pixel_width;
-
-            z_x = 0.0;
-            z_y = 0.0;
-
-            z_x_squared = 0.0;
-            z_y_squared = 0.0;
-
-            for (iteration = 0; iteration < iteration_max &&
-                                ((z_x_squared + z_y_squared) < escape_radius_squared);
-                 iteration++)
-            {
-                z_y = 2 * z_x * z_y + c_y;
-                z_x = z_x_squared - z_y_squared + c_x;
-
-                z_x_squared = z_x * z_x;
-                z_y_squared = z_y * z_y;
-            };
-            result[counter++] = iteration;
-            result[counter++] = i_x;
-            result[counter++] = i_y;
-        };
-    };
-
-    MPI_Send(result, counter, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
-};
 
 void init_ompi_data(struct process_args *t_data, int n_process)
 {
@@ -214,8 +173,8 @@ void init_ompi_data(struct process_args *t_data, int n_process)
     int IMAGE_SIZE = image_size;
     int ver_quadrant_size, hor_quadrant_size, lin, col;
 
-    ver_quadrant_size = (IMAGE_SIZE / (int)sqrt(n_process)) + 1;
-    hor_quadrant_size = (IMAGE_SIZE / (int)sqrt(n_process)) + 1;
+    ver_quadrant_size = (IMAGE_SIZE / (int)sqrt(n_process - 1)) + 1;
+    hor_quadrant_size = (IMAGE_SIZE / (int)sqrt(n_process - 1)) + 1;
 
     if (DEBUG)
     {
@@ -226,14 +185,6 @@ void init_ompi_data(struct process_args *t_data, int n_process)
     {
         for (col = 0; col < IMAGE_SIZE; col += hor_quadrant_size)
         {
-            t_data[t].start_x = col;
-            t_data[t].end_x = min(col + hor_quadrant_size, IMAGE_SIZE);
-            t_data[t].start_y = lin;
-            t_data[t].end_y = min(lin + ver_quadrant_size, IMAGE_SIZE);
-            if (DEBUG)
-                printf("[MASTER]%d: %d %d %d %d\n", t + 1, t_data[t].start_x, t_data[t].end_x,
-                       t_data[t].start_y, t_data[t].end_y);
-            t += 1;
             if (t == n_process - 2) // last process takes the rest of the pixels
             {
                 t_data[t].start_x = col;
@@ -245,6 +196,14 @@ void init_ompi_data(struct process_args *t_data, int n_process)
                            t_data[t].start_y, t_data[t].end_y);
                 return;
             }
+            t_data[t].start_x = col;
+            t_data[t].end_x = min(col + hor_quadrant_size, IMAGE_SIZE);
+            t_data[t].start_y = lin;
+            t_data[t].end_y = min(lin + ver_quadrant_size, IMAGE_SIZE);
+            if (DEBUG)
+                printf("[MASTER]%d: %d %d %d %d\n", t + 1, t_data[t].start_x, t_data[t].end_x,
+                       t_data[t].start_y, t_data[t].end_y);
+            t += 1;
         }
     }
 
@@ -257,6 +216,68 @@ void init_ompi_data(struct process_args *t_data, int n_process)
         t_data[i].end_y = -1;
     }
 }
+
+//
+
+// MPI_Send(result, counter, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
+
+
+__device__ int mandelbrot(double c_x, double c_y) {
+    double z_x = 0;
+    double z_y = 0;
+    double z_x_squared = 0;
+    double z_y_squared = 0;
+    double escape_radius_squared = 4;
+    int iteration_max = 200;
+
+    int iteration;
+
+    for (iteration = 0;
+         iteration < iteration_max &&
+         ((z_x_squared + z_y_squared) < escape_radius_squared);
+         iteration++) {
+                z_y = 2 * z_x * z_y + c_y;
+                z_x = z_x_squared - z_y_squared + c_x;
+
+                z_x_squared = z_x * z_x;
+                z_y_squared = z_y * z_y;
+    };
+
+    return iteration;
+}
+
+
+__global__ void compute_mandelbrot(int start_x, int end_x, int start_y, int end_y, int*result,
+    double c_x_min, double c_y_min, double pixel_width, double pixel_height){
+
+    int i_x;
+    int i_y;
+    int iteration;
+    int pos;
+    // unsigned char *image_buffer_red, unsigned char *image_buffer_green, unsigned char *image_buffer_blue,
+
+    i_x = start_x + blockIdx.x*blockDim.x+threadIdx.x;
+    i_y = start_y + blockIdx.y*blockDim.y+threadIdx.y;
+
+
+    double c_y = c_y_min + i_y * pixel_height;
+
+    if(fabs(c_y) < pixel_height / 2){
+                c_y = 0.0;
+    };
+
+    double c_x = c_x_min + i_x * pixel_width;
+
+    iteration = mandelbrot(c_x, c_y);
+
+    pos = 3 * ((i_y - start_y) * (end_x - start_x) + i_x - start_x);
+
+    result[pos] = iteration;
+    result[pos + 1] = i_x;
+    result[pos + 2] = i_y;
+
+}
+
 
 void compute_mandelbrot_ompi(int argc, char *argv[], int num_processes, int rank_process)
 {
@@ -277,13 +298,13 @@ void compute_mandelbrot_ompi(int argc, char *argv[], int num_processes, int rank
 
     if (rank_process == MASTER)
     {
-        struct process_args *processes_data = NULL;
+        struct process_args *processes_data;
 
         /* Process 0 will be a master process. It defines the ammount of work
         each process needs to execute and then sends the data that each process
         will work on*/
 
-        processes_data = malloc(num_processes * sizeof(struct process_args));
+        processes_data =(struct process_args *) malloc(num_processes * sizeof(struct process_args));
         init_ompi_data(processes_data, num_processes);
 
         for (int p = 0; p < num_processes - 1; p++)
@@ -295,14 +316,46 @@ void compute_mandelbrot_ompi(int argc, char *argv[], int num_processes, int rank
         if (DEBUG)
             printf("[%d]: initiated\n", rank_process);
 
-        struct process_args *process_data = malloc(sizeof(struct process_args));
+        struct process_args *process_data =(struct process_args *) malloc(sizeof(struct process_args));
+        int start_x,start_y,end_x,end_y, counter;
+        int* result, *dev_result;
+
+
         MPI_Recv(process_data, 1, mpi_process_data_type, MASTER, 0, MPI_COMM_WORLD,
                  MPI_STATUS_IGNORE);
 
         if (DEBUG)
             printf("[%d]: received data\n", rank_process);
 
-        compute_mandelbrot(process_data, rank_process);
+        start_y = process_data->start_y;
+        end_y = process_data->end_y;
+        start_x = process_data->start_x;
+        end_x = process_data->end_x;
+
+        if(end_x > start_x && end_y > start_y )
+        {
+
+            dim3 dimBlock(block_dim_x, block_dim_y);
+            dim3 dimGrid((int)ceil((end_x-start_x)/dimBlock.x),
+                         (int)ceil((end_y-start_y)/dimBlock.y));
+
+            counter = 3 * (end_x - start_x) * (end_y - start_y);
+
+            result = (int *)malloc(counter * sizeof(int));
+            cudaMalloc((void**)&dev_result, counter * sizeof(int));
+
+            compute_mandelbrot<<<dimGrid,dimBlock>>>(start_x, end_x, start_y,
+                end_y, dev_result, c_x_min, c_y_min, pixel_width, pixel_height);
+
+            cudaMemcpy(result, dev_result, sizeof(int) * counter, cudaMemcpyDeviceToHost);
+            cudaFree(dev_result);
+        }
+        else
+            counter = 0;
+
+
+
+        MPI_Send(result, counter, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
 
         if (DEBUG)
             printf("[%d]: finished computation\n", rank_process);
@@ -319,8 +372,10 @@ void compute_mandelbrot_ompi(int argc, char *argv[], int num_processes, int rank
             int count;
             MPI_Probe(p, 0, MPI_COMM_WORLD, &status);
             MPI_Get_count(&status, MPI_INT, &count);
+
             if (DEBUG)
                 printf("[MASTER]: process %d had count %d\n", p, count);
+
             int *buffer = (int *)malloc(count * sizeof(int));
             MPI_Recv(buffer, count, MPI_INT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             counters[p] = count;
@@ -355,11 +410,32 @@ void compute_mandelbrot_ompi(int argc, char *argv[], int num_processes, int rank
 
 int main(int argc, char *argv[])
 {
+
     int num_processes, rank_process;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank_process);
+
+    if (rank_process == MASTER)
+    {
+        timer.c_start = clock();
+        clock_gettime(CLOCK_MONOTONIC, &timer.t_start);
+        gettimeofday(&timer.v_start, NULL);
+    }
+
     init(argc, argv, rank_process);
     compute_mandelbrot_ompi(argc, argv, num_processes, rank_process);
+
+    if (rank_process == MASTER)
+    {
+        timer.c_end = clock();
+        clock_gettime(CLOCK_MONOTONIC, &timer.t_end);
+        gettimeofday(&timer.v_end, NULL);
+
+        printf("%f\n",
+               (double)(timer.t_end.tv_sec - timer.t_start.tv_sec) +
+                   (double)(timer.t_end.tv_nsec - timer.t_start.tv_nsec) / 1000000000.0);
+    }
+
     return 0;
 };
