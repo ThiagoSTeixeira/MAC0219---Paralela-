5c6e0b13af654827011838b70f175a95e90f934f
Caio <caiofcastro@usp.br>
2020-07-10 13:46:44 -0300

    add EP1 implementations to run_experiments

--- EP2/run_experiments.py
+++ EP2/run_experiments.py
@@ -1,11 +1,73 @@
 import subprocess
 
 
+def run_seq(repetitions=15, size=4096):
+    subprocess.run(['make', 'mandelbrot_seq'])
+    string = "duration\n"
+    print("[sequential]: starting experiments")
+    for _ in range(repetitions):
+        res = subprocess.run(['./mandelbrot_seq',
+                              "-0.188", "-0.012", "0.554", "0.754",
+                              str(size)],
+                             capture_output=True)
+        string += str(float(res.stdout))+'\n'
+
+    with open('seq_experiments.csv', 'w+') as handle:
+        handle.write(string)
+        handle.close()
+    print("[sequential]: ending experiments")
+
+
+def run_omp(repetitions=15, thread_range=[1, 2, 4, 8, 16, 32, 64],
+            size=4096):
+    subprocess.run(['make', 'mandelbrot_omp'])
+    string = "threads,duration\n"
+    print("[omp]: starting experiments")
+    for t in thread_range:
+        print("[omp]:", t, "threads")
+        for _ in range(repetitions):
+            res = subprocess.run(['./mandelbrot_omp',
+                                  "-0.188", "-0.012", "0.554", "0.754",
+                                  str(size), str(t)],
+                                 capture_output=True)
+            string += str(t)+','
+            string += str(float(res.stdout))+'\n'
+
+    with open('omp_experiments.csv', 'w+') as handle:
+        handle.write(string)
+        handle.close()
+
+    print("[omp]: ending experiments")
+
+
+def run_pth(repetitions=15, thread_range=[1, 2, 4, 8, 16, 32, 64],
+            size=4096):
+    subprocess.run(['make', 'mandelbrot_pth'])
+    string = "threads,duration\n"
+    print("[pthreads]: starting experiments")
+    for t in thread_range:
+        print("[pthreads]:", t, "threads")
+        for _ in range(repetitions):
+            res = subprocess.run(['./mandelbrot_pth',
+                                  "-0.188", "-0.012", "0.554", "0.754",
+                                  str(size), str(t)],
+                                 capture_output=True)
+            string += str(t)+','
+            string += str(float(res.stdout))+'\n'
+
+    with open('pth_experiments.csv', 'w+') as handle:
+        handle.write(string)
+        handle.close()
+
+    print("[pthreads]: ending experiments")
+
+
 def run_cuda(repetitions=15, dimensions=[(2, 2), (4, 4), (8, 8), (16, 16), (32, 32)],
              size=4096):
     subprocess.run(['make', 'mandelbrot_cuda'])
     string = "dimensions,duration\n"
     for x, y in dimensions:
+        print('[cuda]: running for: ', x, y)
         for _ in range(repetitions):
             res = subprocess.run(['./mandelbrot_cuda',
                                   "-0.188", "-0.012", "0.554", "0.754",
@@ -23,6 +85,7 @@ def run_ompi(repetitions=15, process_range=[2, 3, 5, 9, 17, 33, 65], size=4096):
     subprocess.run(['make', 'mandelbrot_ompi'])
     string = "processes,duration\n"
     for p in process_range:
+        print('[ompi]: running for: ', p)
         for _ in range(repetitions):
             res = subprocess.run([
                 "mpirun", "-np", str(p), "--host", "localhost:" +
@@ -44,6 +107,7 @@ def run_ompi_omp(repetitions=15, process_range=[2, 3, 5, 9, 17, 33, 65],
     string = "processes,threads,duration\n"
     for p in process_range:
         for t in thread_range:
+            print('[ompi_omp]: running for: ', p, t)
             for _ in range(repetitions):
                 res = subprocess.run([
                     "mpirun", "-np", str(p), "--host", "localhost:" + str(p),
@@ -65,6 +129,7 @@ def run_ompi_cuda(repetitions=15, process_range=[2, 3, 5, 9, 17, 33, 65],
     string = "processes,dimensions,duration\n"
     for p in process_range:
         for x, y in dimensions:
+            print('[ompi_cuda]: running for: ', p, x, y)
             for _ in range(repetitions):
                 res = subprocess.run([
                     "mpirun", "-np", str(p), "--host", "localhost:" + str(p),
@@ -81,7 +146,10 @@ def run_ompi_cuda(repetitions=15, process_range=[2, 3, 5, 9, 17, 33, 65],
 
 
 if __name__ == "__main__":
+    run_seq()
+    run_pth()
+    run_omp()
     run_cuda()
     run_ompi()
     run_ompi_omp()
-    # run_cuda_ompi()
+    run_ompi_cuda()
